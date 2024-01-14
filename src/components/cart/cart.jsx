764e7231b6342c7657f331f43844089edd2844f1
Silent Ghost <abirloading@gmail.com>
2024-01-14 10:23:27 +0600

    Change Cart Components BackgroundColor

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,26 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import './cart.css'
+import React from "react";
+import "./cart.css";
 const Cart = (props) => {
-    let total = 0;
-    let about ='';
-    for (const item of props.cart) {
-        // console.log(item);
-        total = total + parseInt(item.readTime);
-        about =item.about;
+  let total = 0;
+  let about = "";
+  for (const item of props.cart) {
+    // console.log(item);
+    total = total + parseInt(item.readTime);
+    about = item.about;
+  }
 
-    }
-
-    return (
-        <div className='cartContainer'>
-            <h4 className='cartTittle'>Spent time on read :{total} min</h4>
-            <div className='cartSubTittle'>
-                <h2 className='bookmark'>Bookmarked Blogs :{props.cart.length} </h2>
-                <p className='UserAbout'>{about}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="cartContainer">
+      <h4 className="cartTittle">Spent time on read:{total} min</h4>
+      <div className="cartSubTittle">
+        <h2 className="bookmark">Bookmarked Blogs:{props.cart.length} </h2>
+        <p className="UserAbout">{about}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
