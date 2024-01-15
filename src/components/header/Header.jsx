8b8a68f9bccc14c6a0cfd6d330b7523fca368dfa
Silent Ghost <abirloading@gmail.com>
2024-01-15 22:42:20 +0600

    add popUp Function

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,65 @@
-import './Header.css'
-import profile from '../../images/shahed.jpg'
+import "./Header.css";
+import profile from "../../images/shahed.jpg";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faSignOut,
+  faGear,
+  faQuestion,
+  faMoon,
+} from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
-    return (
-        <div className='headerContainer'>
-            <div className='headerTittle'>
-                <h3>Knowledge World</h3>
-            </div>
-            <div className='headerSubTittle'>
-                <a href="#">Main</a>
-                <a href="#">Learn</a>
-                <a href="#">Blogs</a>
-                <a href="#">Login</a>
-                <img src={profile} alt="" />
-            </div>
+  // imgPopUp_Function
+  const [condition, setCondition] = useState(false);
+  const popupClickHandler = () => {
+    setCondition(!condition);
+  };
+  // AppearancePopUp_Function
+  const [mode, setMode] = useState(false);
+  const underPopUp = () => {
+    setMode(!mode);
+  };
+  const toggleId = mode ? "dark" : "light";
+  return (
+    <div>
+      <div className="headerContainer">
+        <div className="headerTittle">
+          <img onClick={popupClickHandler} src={profile} alt="" />
+          <h3>Knowledge World</h3>
         </div>
-    );
+        <div className="headerSubTittle">
+          <a href="#">Main</a>
+          <a href="#">Learn</a>
+          <a href="#">Blogs</a>
+          <a href="#">Login</a>
+        </div>
+      </div>
+      <div className="Container">
+        {condition ? (
+          <div className="popupContainer">
+            <p>
+              <FontAwesomeIcon icon={faSignOut} /> Sign Out
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faGear} /> Settings
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faQuestion} /> Help
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faEnvelope} /> Send Feedback
+            </p>
+            <p onClick={underPopUp} id={toggleId}>
+              <FontAwesomeIcon icon={faMoon} /> Appearance
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
